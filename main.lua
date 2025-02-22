de8f60dd30db0ab778c35f4f585c91cf1607f06f
BlazZ1t <klimspy@gmail.com>
2025-02-22 21:59:45 +0300

    Added comments to main.lua

--- main.lua
+++ main.lua
@@ -3,17 +3,22 @@
 -- VIRTUAL_WIDTH, VIRTUAL_HEIGHT = 1920 * 0.8, 1080 * 0.8
 
 function love.load()
+    --Add required variables
     anim8 = require 'libraries/anim8'
     sti = require 'libraries/sti'
     push = require('libraries.push')
     camera = require('libraries.camera')
-
+    
+    --Camera initializer
     cam = camera()
 
+    --Set camera to the fullscreen and to the monitor dimensions
     love.window.setMode(0, 0)
 
+    --Load gamemap
     gameMap = sti('maps/12.lua')
 
+    --Load sound effects and music
     sounds = {}
     sounds.main_theme = love.audio.newSource("sounds/MainTheme.ogg", "stream")
     sounds.miku_attack = love.audio.newSource("sounds/kamehameha.swf.ogg", "static")
@@ -27,7 +32,7 @@ function love.load()
 
     background = love.graphics.newImage('assets/background.png')
     
-
+    --Initialize teto table. All the teto parameters are in there
     teto = {}
     teto.sprite_sheet = love.graphics.newImage('assets/sprite_sheet.png')
     teto.pear_sprite_sheet = love.graphics.newImage('assets/sprite_sheet_pear_form.png')
@@ -38,12 +43,15 @@ function love.load()
     teto.y = 150
     teto.speed = 30
 
+
     teto.animations = {}
+    --Load animations for normal form movement using anim8
     teto.animations.down = anim8.newAnimation( teto.grid('1-4', 1), 0.15)
     teto.animations.up = anim8.newAnimation( teto.grid('1-4', 2), 0.15)
     teto.animations.left = anim8.newAnimation( teto.grid('1-4', 3), 0.15 )
     teto.animations.right = anim8.newAnimation( teto.grid('1-4', 4), 0.15 )
 
+    --Load animations for pear form movement using anim8
     teto.animations.pear_right = anim8.newAnimation( teto.pear_grid('1-8', 1), 0.15 )
     teto.animations.pear_left = anim8.newAnimation( teto.pear_grid('1-8', 2), 0.15 )
     teto.animations.pear_up = anim8.newAnimation(teto.pear_grid('1-8', 3), 0.15)
@@ -54,6 +62,7 @@ function love.load()
 
     teto.current_sprite_sheet = teto.sprite_sheet
 
+    --Initialize miku table
     miku = {}
     miku.attack_sprite_sheet = love.graphics.newImage('assets/miku_attack_sprite_sheet.png')
     miku.grid = anim8.newGrid(32, 48, miku.attack_sprite_sheet:getWidth(), miku.attack_sprite_sheet:getHeight())
@@ -67,7 +76,7 @@ end
 function love.update(dt)
 
     local isMoving = false
-
+    --Check movement inputs as well as pear form input
     if love.keyboard.isDown("w") then
         isMoving = true
         teto.y = teto.y - teto.speed
@@ -115,13 +124,13 @@ function love.update(dt)
             teto.current_sprite_sheet = teto.sprite_sheet
         end
     end
-
+    --Miku attack trigger (for now place enter to trigger)
     if love.keyboard.isDown("return") then
         sounds.miku_attack:play()
         miku.isAttacking = true
         miku.anim = miku.animations.attack
     end
-
+    --Set teto to frame 1 of the animation, if she is still
     if isMoving == false then
         teto.anim:gotoFrame(1)
     end
