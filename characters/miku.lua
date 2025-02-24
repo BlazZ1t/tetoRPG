2f015c3b80aba3d1fb3ac396ba0caca1f2221e8a
BlazZ1t <klimspy@gmail.com>
2025-02-24 04:16:29 +0300

    Fixed miku animation, now properly stops at the end

--- characters/miku.lua
+++ characters/miku.lua
@@ -6,7 +6,7 @@ function miku.getMiku()
     miku_table.attack_sprite_sheet = love.graphics.newImage('assets/images/miku_attack_sprite_sheet.png')
     miku_table.grid = anim8.newGrid(32, 48, miku_table.attack_sprite_sheet:getWidth(), miku_table.attack_sprite_sheet:getHeight())
     miku_table.animations = {
-        attack = anim8.newAnimation(miku_table.grid('1-26', 1), 0.23),
+        attack = anim8.newAnimation(miku_table.grid('1-26', 1), 0.23, { pauseAtEnd = true }),
     }
     miku_table.anim = miku_table.animations.attack
     miku_table.isAttacking = false
